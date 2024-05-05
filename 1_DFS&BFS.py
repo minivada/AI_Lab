24d84c40e01b3c40464bb16eb35141fe2163ea19
minivada <167669311+minivada@users.noreply.github.com>
2024-05-05 20:56:56 +0530

    Update 1_DFS&BFS.py

--- 1_DFS&BFS.py
+++ 1_DFS&BFS.py
@@ -1,41 +1,55 @@
 def dfs(visited,graph,node):
     if node not in visited:
-        print(node,end=" ")
+        print(node,end = " ")
+        visited.add(node)
         for neighbour in graph[node]:
-            dfs(visited,graph,neighbour)
+            dfs(visited, graph, neighbour)
+
 def bfs(visited,graph,node,queue):
     visited.add(node)
     queue.append(node)
+
     while queue:
-        s=queue.pop(0)
-        print(s,end=" ")
+        s = queue.pop(0)
+        print(s,end = " ")
         for neighbour in graph[s]:
             if neighbour not in visited:
                 visited.add(neighbour)
                 queue.append(neighbour)
+
 def main():
-    visited_dfs=set()
-    visited_bfs=set()
-    queue=[]
-    graph={
-        1:[2,3],
-        2:[4,5],
-        3:[6,7],
-        4:[],
-        5:[],
-        6:[],
-        7:[]
 
-    }
-    print("the following dfs")
-    dfs(visited_dfs,graph,1)
+    visited1 = set()  # To keep track of DFS visited nodes
+    visited2 = set()  # To keep track of BFS visited nodes
+    queue = []        # For BFS
+    n = int(input("Enter number of nodes: "))
+    graph = {}
+
+    for i in range(1, n+1):
+        edges = int(input("Enter number of edges for node {}: ".format(i)))
+        graph[i] = []
+        for j in range(edges):
+            node = int(input("Enter edge {} for node {}: ".format(j+1, i)))
+            graph[i].append(node)
+
+    print("The following is DFS:")
+    dfs(visited1, graph, 1)
     print()
-    print("the following Bfs")
-    bfs(visited_bfs,graph,1,queue)
+    print("The following is BFS:")
+    bfs(visited2, graph, 1, queue)
 
-
-if __name__=="__main__":
+if _name_ == "_main_":
     main()
 
-
-    
+#     Enter number of nodes: 4
+# Enter number of edges for node 1: 2
+# Enter edge 1 for node 1: 2
+# Enter edge 2 for node 1: 3
+# Enter number of edges for node 2: 1
+# Enter edge 1 for node 2: 4
+# Enter number of edges for node 3: 0
+# Enter number of edges for node 4: 0
+# The following is DFS:
+# 1 2 4 3
+# The following is BFS:
+# 1 2 3 4
